fe11eacd79f581549821d323e81e08306386726a
Khanh Hoang <hoangkhanh@Khanhs-MacBook-Air.local>
2021-10-22 00:13:39 -0400

    Finished projects section, working with footer

--- assets/js/main.js
+++ assets/js/main.js
@@ -62,3 +62,16 @@ tabs.forEach(tab => {
         tab.classList.add('qualification__active')
     })
 })
+
+let swiper = new Swiper(".project__container", {
+    cssMode: true,
+    loop: true,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+})
