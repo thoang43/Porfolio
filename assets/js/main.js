29d8fc360128fe2cfe4ff237341b6becbcf9807b
Khanh Hoang <hoangkhanh@Khanhs-MacBook-Air.local>
2021-10-22 14:23:52 -0400

    Adding features for screen size, need to fix bug on bubble and header icons

--- assets/js/main.js
+++ assets/js/main.js
@@ -43,6 +43,8 @@ skillsHeader.forEach((el)=>{
     el.addEventListener('click', toggleSkills)
 })
 
+
+/* List of projects and put them in swiper*/
 const tabs = document.querySelectorAll('[data-target]'),
 tabContents = document.querySelectorAll('[data-content]')
 
@@ -75,3 +77,39 @@ let swiper = new Swiper(".project__container", {
       clickable: true,
     },
 })
+
+/* Scrolling page */ 
+const sections = document.querySelectorAll('section[id]')
+
+function scrollActive(){
+    const scrollY = window.pageYOffset
+
+    sections.forEach(current =>{
+        const sectionHeight = current.offsetHeight
+        const sectionTop = current.offsetTop - 50;
+        sectionId = current.getAttribute('id')
+
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
+        }else{
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
+        }
+    })
+}
+window.addEventListener('scroll', scrollActive)
+
+/* Background header */
+function scrollHeader(){
+    const nav = document.getElementById('header')
+    // When the scroll is greater than 80 viewport height, add the scroll-header class to the header tag
+    if(this.scrollY >= 80) nav.classList.add('scroll-header'); else nav.classList.remove('scroll-header')
+}
+window.addEventListener('scroll', scrollHeader)
+
+/* Show scroll up */
+function scrollUp(){
+    const scrollUp = document.getElementById('scroll-up');
+    // When the scroll is higher than 560 viewport height, add the show-scroll class to the a tag with the scroll-top class
+    if(this.scrollY >= 560) scrollUp.classList.add('show-scroll'); else scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
